56e7228b3364860512b05c41adc6a8c74a3556c5
IntisN <Intis.Neviero@gmail.com>
2017-12-19 20:57:13 +0200

    Added some variables to Hello World app

--- RCS/HelloWorld/Program.cs
+++ RCS/HelloWorld/Program.cs
@@ -10,8 +10,13 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(value: "Please enter your name.");
+            //Adding variable with my name
+            string myName;
+            //Adding data to created variable
+            myName = Console.ReadLine();
             //Printing Hello World on screen
-            Console.WriteLine(value: "Hello World! Es esmu te!");
+            Console.WriteLine(value: "Hello World! Es esmu te! " + myName);
             //Pausing app so we could read this text
             Console.ReadLine();
         }
