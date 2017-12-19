add09960ccf24c55411303dd67ece3d8d7313802
IntisN <Intis.Neviero@gmail.com>
2017-12-19 20:25:49 +0200

    created first Hello World!

---
+++ RCS/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Printing Hello World on screen
+            Console.WriteLine(value: "Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
