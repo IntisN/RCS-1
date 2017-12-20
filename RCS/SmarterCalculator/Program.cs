bdda25bb277edf7408ee578bead4fdc57efef6df
IntisN <Intis.Neviero@gmail.com>
2017-12-20 21:14:20 +0200

    Added smarter calculator w/o success

---
+++ RCS/SmarterCalculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmarterCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating calculator object
+            //Asking banana to enter calculation
+            Console.WriteLine("Please enter calculation");
+            string input = Console.ReadLine();
+            //
+            int result;
+            int counter = 0;
+            while (counter < input.Length)
+            {
+                char symbol = input[counter];
+                if (symbol == '+')
+                {
+
+                }
+                else
+                {
+                    int number;
+                    number = Int32.Parse(symbol.ToString());
+                    Console.WriteLine("Number" + number);
+                }
+
+                input = input.Remove(1, input.Length - 1);
+                counter = counter + 1;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
