5406877ce0cf3acea22072ff48c63823849aef87
IntisN <Intis.Neviero@gmail.com>
2017-12-20 20:11:37 +0200

    Added calculator

---
+++ RCS/Kalkulators/Calculations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulators
+{
+    class Calculations
+    {
+        public int AskBananaForNumber()
+        {
+            //Print text asking for a number
+            Console.WriteLine("Enter a number, banana!");
+            //Read number from console
+            //Define variable for a number
+            int number;
+            //Entering number in variable
+            string inputText = Console.ReadLine();
+            //Checking if number is entered
+            bool success = Int32.TryParse(inputText, out number);
+
+            if (success == false)
+            {
+                //Warning banana that wrong value is entered
+                Console.WriteLine("Wrong value entered! :p");
+                //Returning to function
+                number = this.AskBananaForNumber();
+            }
+
+            //Return number to function
+            return number;
+        }
+    }
+}
---
+++ RCS/Kalkulators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating calculation object
+            Calculations calc;
+            calc = new Calculations();
+            //Askig banana for a first value
+            int FirstNumber = calc.AskBananaForNumber();
+            //Asking banana for a second value
+            int SecondNumber = calc.AskBananaForNumber();
+            //sum
+            int result = FirstNumber + SecondNumber;
+            //Print result on screen
+            Console.WriteLine("Result is: ");
+            Console.WriteLine(result);
+            //Pausing app
+            Console.ReadLine();
+        }
+    }
+}
