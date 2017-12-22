c497d88864662e7642b72ae2f33c4e576f921c46
IntisN <Intis.Neviero@gmail.com>
2017-12-22 20:17:21 +0200

    Created possibility to save list and run when open app

--- RCS/BuyList/MainWindow.xaml.cs
+++ RCS/BuyList/MainWindow.xaml.cs
@@ -15,7 +15,10 @@ using System.Windows.Shapes;
 
 namespace BuyList
 {
+    //For ObservableCollection using
     using System.Collections.ObjectModel;
+    //Allow to create output file - using "file"
+    using System.IO;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -28,10 +31,10 @@ namespace BuyList
             InitializeComponent();
 
             //Asking for adding some items
-            this.BuyListItemName.Text = "Lūdzu, ievadiet pirkumu";
-            //Hardcoding some text in ItemsControl
-            this.BuyItemsList.Add("Āboli");
-            this.BuyItemsList.Add("Bumbieri");
+            this.BuyListItemName.Text = "Lūdzu, ievadiet pirkumu!";
+            //Hardcoding some text in ItemsControl from history
+            string history = File.ReadAllText(@".\Saraksts.txt");
+            this.BuyItemsList.Add(history);
             //Add entered items to ItemsControl
             this.BuyItemsListControl.ItemsSource = this.BuyItemsList;
         }
@@ -51,7 +54,24 @@ namespace BuyList
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            //Deleting everything from BuyItemsList
             this.BuyItemsList.Clear();
+            //Deleting everything from BuyItemName box
+            //this.BuyItemName.Text = "";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //Write list to file
+            File.WriteAllLines(@".\Saraksts.txt", this.BuyItemsList);
+        }
+
+        private void BuyListItemName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            //Clean BuyListItemName when mouse is clicked in box
+            TextBox tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= BuyListItemName_GotFocus;
         }
     }
 }
