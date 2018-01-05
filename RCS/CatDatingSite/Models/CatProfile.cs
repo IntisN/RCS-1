a8ab44d03e07834c042af37217548e0746595800
IntisN <Intis.Neviero@gmail.com>
2018-01-05 11:36:39 +0200

    Added cat lover site v1.0 some kind of working crap

---
+++ RCS/CatDatingSite/Models/CatProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CatDatingSite.Models
+
+    
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CatProfile
+
+    { [Key]
+        public int CatID { get; set; }
+        [Required]
+        public string CatName { get; set; }
+        public int CatAge { get; set; }
+        public string CatImage { get; set; }
+        public string CatDescription { get; set; }
+    }
+}
